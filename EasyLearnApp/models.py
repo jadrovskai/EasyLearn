5f8152a4240d95405122a70cc9deaebfdd9558fa
Iva Jadrovska <iva@embedsocial.com>
2022-09-04 14:50:42 +0200

    fix

--- EasyLearnApp/models.py
+++ EasyLearnApp/models.py
@@ -11,4 +11,4 @@ class Video(models.Model):
     id = models.AutoField(primary_key=True)
     video_name = models.CharField(max_length=50)
     video_image = models.ImageField(upload_to="cover_images/", null=True, blank=True)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE,  null=True, blank=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE,  null=True, blank=True) 

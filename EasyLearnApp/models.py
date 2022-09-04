d5eb617aefcfeb276bd8708223e21c207f355a3b
Iva Jadrovska <iva@embedsocial.com>
2022-09-04 14:32:06 +0200

    migration added

--- EasyLearnApp/models.py
+++ EasyLearnApp/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Subject(models.Model):
+    id = models.AutoField(primary_key=True)
     subject_name = models.CharField(max_length=50)
     subject_image = models.ImageField(upload_to="cover_images/", null=True, blank=True)
     
 class Video(models.Model):
+    id = models.AutoField(primary_key=True)
     video_name = models.CharField(max_length=50)
     video_image = models.ImageField(upload_to="cover_images/", null=True, blank=True)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE,  null=True, blank=True)

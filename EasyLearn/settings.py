1c859ef26aa3049e0d5054561ae4abe6a2ef9e44
Iva Jadrovska <iva@embedsocial.com>
2022-09-04 15:38:15 +0200

    fix deploy

--- EasyLearn/settings.py
+++ EasyLearn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&rzqkp@i0cvbpek!mrky9s&x)rm*bno--ewotya&2!3__sev-_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
